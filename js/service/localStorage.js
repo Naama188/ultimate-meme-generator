e4fb8e8c78bb1979ce75558a407503728cfe309e
Naama Tanne <naama.t@gmail.com>
2024-12-22 19:45:49 +0200

    Setup storage & utils files

---
+++ js/service/localStorage.js
@@ -0,0 +1,11 @@
+'user strict'
+
+function saveToStorage(key, value) {
+    const json = JSON.stringify(value)
+    localStorage.setItem(key, json)
+}
+
+function loadFromStorage(key) {
+    const json = localStorage.getItem(key)
+    return JSON.parse(json)
+}
